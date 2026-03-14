b1147fca2285233bfe0b111750c9effc66d9fe27
pavanratnakar07 <127281384+pavanratnakar07@users.noreply.github.com>
2026-03-14 13:32:15 +0530

    Add files via upload

---
+++ NumberPattern.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class NumberPattern {
+
+    public static void main(String [] args) {
+        int count = 1;
+
+        for (int rows = 1; rows <= 5; rows++) {
+            for (int col = 1; col <= rows; col++) {
+                System.out.print(count);
+                if (col < rows) {
+                    System.out.print(" ");
+                }
+                count++;
+            }
+            System.out.println();
+        }
+
+    }
+
+}
